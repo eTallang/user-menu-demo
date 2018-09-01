69250e190aa73ee3bae4602a009e7e28a0bb78ef
Erik Tallang <e.tallang@gmail.com>
2018-09-01 23:09:15 +0200

    feat(user-menu): implement user menu

--- src/app/user-menu/user-menu.component.ts
+++ src/app/user-menu/user-menu.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-user-menu',
   templateUrl: './user-menu.component.html',
-  styleUrls: ['./user-menu.component.scss']
+  styleUrls: ['./user-menu.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  animations: [
+    trigger('opacity', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+          transform: 'scale(.97)'
+        })
+      ),
+      state(
+        '*',
+        style({
+          opacity: 1,
+          transform: 'scale(1)'
+        })
+      ),
+      transition('void <=> *', animate('500ms cubic-bezier(.1, 0, 0, 1)')),
+    ])
+  ]
 })
-export class UserMenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class UserMenuComponent {
+  isOpen = false;
 }
