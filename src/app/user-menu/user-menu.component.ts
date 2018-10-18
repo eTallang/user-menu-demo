c679ded354d89bd39bed8c3ebaf5b6a0d10937cf
Erik Tallang <e.tallang@gmail.com>
2018-10-18 22:22:08 +0200

    feat(user-menu): add defaults for demo

--- src/app/user-menu/user-menu.component.ts
+++ src/app/user-menu/user-menu.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-user-menu',
   templateUrl: './user-menu.component.html',
-  styleUrls: ['./user-menu.component.scss']
+  styleUrls: ['./user-menu.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class UserMenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class UserMenuComponent {
 
 }
